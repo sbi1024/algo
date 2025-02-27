8bedecf0554fb4071146cf204c5f9b0cbd4ac890
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-02-27 16:08:32 +0900

    임시저장

--- src/Main.java
+++ src/Main.java
@@ -2116,118 +2116,263 @@ public class Main {
 //    }
 //}
 
+// https://www.acmicpc.net/problem/14889
+//import java.io.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.StringTokenizer;
+//
+//class Main {
+//    static int N; // 입력값으로 주어지는 인원의 수
+//    static int[] people; // 인원 (N)
+//    static int[] selectPeople; // 선택된 인원 (N / 2)
+//    static List<Integer> notSelectPeople = new ArrayList<>(); // 비 선택된 인원 (N / 2)
+//    static boolean[] visitPeople; // 방문한 인원 (N)
+//    static int[][] peoplePowerMap; // 각 인원이 시너지가 존재하는 능력치를 담는 map
+//
+//    static int result = Integer.MAX_VALUE; // 결과값 변수 선언
+//
+//    public static void main(String[] args) throws IOException {
+//        // 입출력 스트림 생성
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st;
+//
+//        // 변수 값 할당
+//        N = Integer.parseInt(br.readLine());
+//        people = new int[N];
+//        selectPeople = new int[N / 2];
+//        visitPeople = new boolean[N];
+//        peoplePowerMap = new int[N][N];
+//        for (int i = 0; i < N; i++) {
+//            people[i] = i;
+//            st = new StringTokenizer(br.readLine());
+//            for (int j = 0; j < N; j++) {
+//                peoplePowerMap[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//
+//        // 주어진 인원의 조합 계산 (N개 중에 N / 2를 뽑음)
+//        peopleComb(0, 0);
+//
+//
+//        // 결과값 출력 및 자원 반환
+//        bw.write(result + "");
+//        bw.flush();
+//        bw.close();
+//        br.close();
+//    }
+//
+//    // 뽑을수 있는 사람의 조합을 구함
+//    static void peopleComb(int depth, int startIndex) {
+//        // 기저 조건 설정 > 뽑아야 하는 인원 수에 도달할 경우 종료
+//        if (depth == selectPeople.length) {
+//            // 여기서 반대로 뽑히지 않은 인원과의 차이값을 계산
+//            initNotSelectPeople();
+//            // 최소값 갱신
+//            updateMinValue();
+//            // 기저 조건 완료 = 메서드 종료 처리
+//            return;
+//        }
+//
+//        // 조합 반복문 dfs
+//        for (int i = startIndex; i < N; i++) {
+//            if (!visitPeople[i]) {
+//                selectPeople[depth] = people[i];
+//                visitPeople[i] = true;
+//                peopleComb(depth + 1, i + 1);
+//                visitPeople[i] = false;
+//            }
+//        }
+//    }
+//
+//    static void initNotSelectPeople() {
+//        // 혹시 이전에 사용된 데이터가 있을수 있기에 clear 처리
+//        notSelectPeople.clear();
+//        // selectPeople 이 아닌 배열을 만듬
+//        for (int i = 0; i < visitPeople.length; i++) {
+//            if (!visitPeople[i]) notSelectPeople.add(i);
+//        }
+//    }
+//
+//    static void updateMinValue() {
+//        // selectPeople, notSelectPeople 로 두가지 팀은 나누어 졌음
+//        // 다만 해당 변수에 대해서 2가지를 뽑는 다는 조합의 경우의 수를 구해서 합산한 값을 각각 구해야 함
+//        int abs = Math.abs(calcSelectPeople() - calcNotSelectPeople());
+//        // 도출된 값 갱신
+//        if (abs < result) result = abs;
+//    }
+//
+//    // 선택된 인원의 능력치 합 계산
+//    static int calcSelectPeople() {
+//        int sum = 0;
+//        int length = selectPeople.length;
+//        for (int i = 0; i < length; i++) {
+//            for (int j = i + 1; j < length; j++) {
+//                int first = selectPeople[i];
+//                int second = selectPeople[j];
+//                sum += peoplePowerMap[first][second] + peoplePowerMap[second][first];
+//            }
+//        }
+//        return sum;
+//    }
+//
+//    // 비 선택된 인원의 능력치 합 계산
+//    static int calcNotSelectPeople() {
+//        int sum = 0;
+//        int size = notSelectPeople.size();
+//        for (int i = 0; i < size; i++) {
+//            for (int j = i + 1; j < size; j++) {
+//                int first = notSelectPeople.get(i);
+//                int second = notSelectPeople.get(j);
+//                sum += peoplePowerMap[first][second] + peoplePowerMap[second][first];
+//            }
+//        }
+//        return sum;
+//    }
+//}
+
+// https://www.acmicpc.net/problem/14888
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Main {
-    static int N; // 입력값으로 주어지는 인원의 수
-    static int[] people; // 인원 (N)
-    static int[] selectPeople; // 선택된 인원 (N / 2)
-    static List<Integer> notSelectPeople = new ArrayList<>(); // 비 선택된 인원 (N / 2)
-    static boolean[] visitPeople; // 방문한 인원 (N)
-    static int[][] peoplePowerMap; // 각 인원이 시너지가 존재하는 능력치를 담는 map
+// 연산자의 순열을 구해야 한다.
+// 연산자의 순열의 경우의 수 마다 결과값을 계산해서 최대값, 최소값을 갱신한다.
 
-    static int result = Integer.MAX_VALUE; // 결과값 변수 선언
+public class Main {
+    static int N; // N개로 이어진 수열 (EX) 1, 2 ...)
+    static int[] numberArray; // N개로 이어진 수열을 담는 배열
+
+    // 각 연산자마다 배열을 생성하여 사용하는거보다, 가변 리스트로 담아서 사용하는게 낫겟다는 판단을 함
+    static List<Integer> operationList = new ArrayList<>(); // 연산자를 담는 list
+    static List<Integer> selectOperationList = new ArrayList<>(); // 선택된 연산자를 담는 list
+    static boolean[] operationVisit; // 연산자 방문 여부 확인 배열
+
+    // 결과값으로 출력할 최대값과 최소값 변수 선언
+    static int maxValue = Integer.MIN_VALUE;
+    static int minValue = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-        // 입출력 스트림 생성
+        // 입추력 스트림 생성
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        // 변수 값 할당
+        // 입력값 할당
         N = Integer.parseInt(br.readLine());
-        people = new int[N];
-        selectPeople = new int[N / 2];
-        visitPeople = new boolean[N];
-        peoplePowerMap = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            people[i] = i;
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                peoplePowerMap[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        numberArray = new int[N];
+        // numberArray 값 할당 (피연산자)
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) numberArray[i] = Integer.parseInt(st.nextToken());
 
-        // 주어진 인원의 조합 계산 (N개 중에 N / 2를 뽑음)
-        peopleComb(0, 0);
+        // 연산자 기호 값 할당
+        st = new StringTokenizer(br.readLine());
 
+        // 연산 횟수 계산
+        int plusCount = Integer.parseInt(st.nextToken());
+        int minusCount = Integer.parseInt(st.nextToken());
+        int multiCount = Integer.parseInt(st.nextToken());
+        int modCount = Integer.parseInt(st.nextToken());
 
-        // 결과값 출력 및 자원 반환
-        bw.write(result + "");
+        // list 에 add ( + : 0, - : 1, * : 2, / : 3)
+        for (int i = 0; i < plusCount; i++) operationList.add(0);
+        for (int i = 0; i < minusCount; i++) operationList.add(1);
+        for (int i = 0; i < multiCount; i++) operationList.add(2);
+        for (int i = 0; i < modCount; i++) operationList.add(3);
+
+        // 방문 여부 확인용 변수 선언
+        operationVisit = new boolean[operationList.size()];
+
+        // operationList 의 순열 경우의 수 구하기
+        // 구하고자 하는 경우의 수는 N개 중에 N-1개 고르기
+        perm(0);
+
+        // 결과값 포맷 맞추기 > 최대값 (줄바꿈) 최소값 형식
+        sb.append(maxValue).append("\n");
+        sb.append(minValue).append("\n");
+
+        // 결과값 출력
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
-    // 뽑을수 있는 사람의 조합을 구함
-    static void peopleComb(int depth, int startIndex) {
-        // 기저 조건 설정 > 뽑아야 하는 인원 수에 도달할 경우 종료
-        if (depth == selectPeople.length) {
-            // 여기서 반대로 뽑히지 않은 인원과의 차이값을 계산
-            initNotSelectPeople();
-            // 최소값 갱신
-            updateMinValue();
-            // 기저 조건 완료 = 메서드 종료 처리
+    // 순열 dfs
+    public static void perm(int depth) {
+        // 기저조건 설정
+        if (depth == N - 1) {
+            // 연산 결과 계산 실행
+            calcMinMaxValue();
             return;
         }
 
-        // 조합 반복문 dfs
-        for (int i = startIndex; i < N; i++) {
-            if (!visitPeople[i]) {
-                selectPeople[depth] = people[i];
-                visitPeople[i] = true;
-                peopleComb(depth + 1, i + 1);
-                visitPeople[i] = false;
+        // 연산자를 뽑는 반복문
+        for (int i = 0; i < operationList.size(); i++) {
+            if (!operationVisit[i]) {
+                selectOperationList.add(operationList.get(i));
+                operationVisit[i] = true;
+                perm(depth + 1);
+                operationVisit[i] = false;
+                selectOperationList.remove(selectOperationList.size() - 1);
             }
         }
     }
 
-    static void initNotSelectPeople() {
-        // 혹시 이전에 사용된 데이터가 있을수 있기에 clear 처리
-        notSelectPeople.clear();
-        // selectPeople 이 아닌 배열을 만듬
-        for (int i = 0; i < visitPeople.length; i++) {
-            if (!visitPeople[i]) notSelectPeople.add(i);
+    // 순열로 선택된 연산자 계산처리
+    static void calcMinMaxValue() {
+        // 첫 변수값 할당
+        int result = numberArray[0];
+
+        // 마지막 연산과정은 제외함
+        for (int i = 1; i < numberArray.length - 1; i++) {
+            int number = numberArray[i];
+            int operation = selectOperationList.get(i - 1);
+            result = calc(result, number, operation);
         }
+
+        // 마지막 연산과정 처리
+        result = calc(
+                result,
+                numberArray[numberArray.length - 1],
+                selectOperationList.get(selectOperationList.size() - 1)
+        );
+
+        // 최대값 최소값 계산
+        maxValue = Math.max(result, maxValue);
+        minValue = Math.min(result, minValue);
     }
 
-    static void updateMinValue() {
-        // selectPeople, notSelectPeople 로 두가지 팀은 나누어 졌음
-        // 다만 해당 변수에 대해서 2가지를 뽑는 다는 조합의 경우의 수를 구해서 합산한 값을 각각 구해야 함
-        int abs = Math.abs(calcSelectPeople() - calcNotSelectPeople());
-        // 도출된 값 갱신
-        if (abs < result) result = abs;
-    }
-
-    // 선택된 인원의 능력치 합 계산
-    static int calcSelectPeople() {
-        int sum = 0;
-        int length = selectPeople.length;
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                int first = selectPeople[i];
-                int second = selectPeople[j];
-                sum += peoplePowerMap[first][second] + peoplePowerMap[second][first];
-            }
+    static int calc(int number1, int number2, int operation) {
+        // 리턴값 선언
+        int result = 0;
+        // 연산자 마다 다르게 처리
+        switch (operation) {
+            // 덧셈
+            case 0:
+                result = number1 + number2;
+                break;
+            // 뺄셈
+            case 1:
+                result = number1 - number2;
+                break;
+            // 곱셈
+            case 2:
+                result = number1 * number2;
+                break;
+            // 나눗셈
+            case 3:
+                result = number1 / number2;
+                break;
+            default:
+                break;
         }
-        return sum;
-    }
-
-    // 비 선택된 인원의 능력치 합 계산
-    static int calcNotSelectPeople() {
-        int sum = 0;
-        int size = notSelectPeople.size();
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                int first = notSelectPeople.get(i);
-                int second = notSelectPeople.get(j);
-                sum += peoplePowerMap[first][second] + peoplePowerMap[second][first];
-            }
-        }
-        return sum;
+        // 결과값 반환
+        return result;
     }
 }
+
